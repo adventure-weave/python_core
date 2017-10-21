486e0ddd895c0603d592fbf111a85c450074e3b7
Michał Woźniczak <michal.wozniczak@gmail.com>
2017-10-21 11:43:03 +0200

    First parameter in nodes is now id rather than title

--- adventure_weave/nodes.py
+++ adventure_weave/nodes.py
@@ -3,18 +3,19 @@
 # from adventure_weave.loaders import load_choice
 from adventure_weave.choices import load_choice
 
-def load_node(title, data):
+def load_node(id, data):
     return globals().get(
         data.get('class', 'BaseNode'),
         BaseNode
-    )(title, data)
+    )(id, data)
 
 class BaseNode:
     '''Base functionality all nodes inherit'''
-    def __init__(self, title, data=None):
+    def __init__(self, id, data=None):
         data = data or {}
 
-        self.title = title
+        self.id = id
+        self.title = data.get('title', id)
         self.content = data.get('content', '')
         self.choices = [
             load_choice(self, choice) for choice in (data.get('choices') or [])
@@ -22,10 +23,11 @@ class BaseNode:
 
     def __str__(self):
         '''Returns markdown representation of itself'''
-        return '\n\n'.join([
-            '# ' + self.title,
+        paragraphs = ['# ' + self.title] if self.title else []
+        paragraphs += [
             self.content,
             '\n'.join(
                 '**%s.** %s' % (x+1, choice) for (x, choice) in enumerate(self.choices)
             )
-        ])
+        ]
+        return '\n\n'.join(paragraphs)
