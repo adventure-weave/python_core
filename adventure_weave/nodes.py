c8749956e733690466a954c97ff79af93e3b6ef7
Michał Woźniczak <michal.wozniczak@gmail.com>
2017-10-21 21:25:42 +0200

    Added support for input types (currently only 'pick')

--- adventure_weave/nodes.py
+++ adventure_weave/nodes.py
@@ -11,16 +11,33 @@ def load_node(id, data):
 
 class BaseNode:
     '''Base functionality all nodes inherit'''
+
+    input_types = {'pick', 'type'}
+
     def __init__(self, id, data=None):
         data = data or {}
 
         self.id = id
         self.title = data.get('title', id)
         self.content = data.get('content', '')
+        self.input = data.get('input', 'pick')
+
+        if self.input not in self.input_types:
+            self.input = 'pick'
+
         self.choices = [
             load_choice(self, choice) for choice in (data.get('choices') or [])
         ]
         self.is_start = data.get('is_start', False)
+
+    def interpret(self, received):
+        if self.input == 'pick':
+            try:
+                return self.choices[int(received) - 1].leads_to
+            except:
+                return None
+        elif self.input == 'type':
+            pass
 
     def __str__(self):
         '''Returns markdown representation of itself'''
